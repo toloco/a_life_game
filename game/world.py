894506f76b9c69b68527cb3418b9d4454b943928
Tolo Palmer <toloco@gmail.com>
2015-12-18 07:44:45 +0000

    The game

---
+++ game/world.py
@@ -0,0 +1,215 @@
+import random
+import Levenshtein
+
+from enum import Enum
+
+from scipy.stats import bernoulli
+
+
+from .constants import (
+    REDIS_ZORBS,
+    REDIS_WORLD,
+    DAY_REDUCE_ENERGY,
+    DAY_REDUCE_LIFENESS,
+    ENERGY_QUANTUM
+)
+
+from .zorb import Zorb, get_zorbs, get_random_zorb
+from .inout import get_db
+from .event import EventType, Event
+
+
+class World(object):
+    """Shared instance value
+    """
+    __instance = None
+
+    def __new__(cls, **kwargs):
+        if World.__instance is None:
+            World.__instance = object.__new__(cls)
+        return World.__instance
+
+    def __init__(self, **kwargs):
+        if kwargs:
+            for attribute, value in kwargs.items():
+                setattr(self, attribute, value)
+        else:
+            db = get_db()
+            for attribute, value in db.hgetall(REDIS_WORLD).items():
+                attribute, value = attribute.decode("utf-8"), value.decode("utf-8")
+                setattr(self, attribute, float(value))
+
+    def __del__(self):
+        # Update in Redis
+        db = get_db()
+        db.hmset(REDIS_WORLD, self.__dict__)
+
+    @property
+    def zorbs_no(self):
+        db = get_db()
+        return db.scard(REDIS_ZORBS)
+
+    def next_day(self):
+        self.day += 1
+
+
+class Actions(Enum):
+    """Actions
+    """
+    energy = 1
+    zorb = 2
+    none = 3
+    attack = 4
+    reproduce = 5
+    run = 6
+
+
+def zorb_day(world, zorb):
+
+    def energy_zorb_or_none(world):
+        """
+            p_find_energy = max_energy_day / world_size
+            p_find_zorb = no_zorbs / world_size
+        """
+
+        energy_found = bernoulli.rvs(
+            (world.energy / world.size),
+            size=1
+        )
+        if energy_found[0] == 1:
+            return Actions.energy
+
+        zorb_found = bernoulli.rvs(
+            (world.zorbs_no / world.size),
+            size=1
+        )
+        if zorb_found[0] == 1:
+            return Actions.zorb
+
+        return Actions.none
+
+    def attack_reproduce_or_run(world, zorb_a, zorb_b):
+        def can_attack(zorb_a, zorb_b):
+            attack_defense_ratio = zorb_b.defense / zorb_a.attack
+            speed_ratio = zorb_b.speed / zorb_a.speed
+
+            return (attack_defense_ratio - random.expovariate(lambd=speed_ratio)) > 1
+
+        def can_reproduce(world, zorb_a, zorb_b):
+            """ 2 given zorbs can reproduce if diff is less than a given percentage
+            """
+            return Levenshtein.distance(zorb_a.dna, zorb_b.dna) < world.reproducetibility
+
+        if can_reproduce(world, zorb_a, zorb_b):
+            return Actions.reproduce
+        elif can_attack(zorb_a, zorb_b):
+            return Actions.attack
+        else:
+            return Actions.run
+
+    action = energy_zorb_or_none(world)
+
+    if action == Actions.energy:
+        print("Energy found zid:`{}` total_energy :`{}`".format(zorb.zid, zorb.energy))
+
+        # Create event
+        event_data = zorb.__dict__
+        event_data["feed"] = ENERGY_QUANTUM
+        Event(event_type=EventType.Feed, data=event_data)
+
+        zorb.energy += ENERGY_QUANTUM
+
+    elif action == Actions.zorb:
+        # Meet a zorb
+        found_a_zorb = get_random_zorb()
+
+        action = attack_reproduce_or_run(world, zorb, found_a_zorb)
+
+        if action == Actions.attack:
+            attack(world, zorb, found_a_zorb)
+
+        elif action == Actions.reproduce:
+            reproduce(world, zorb, found_a_zorb)
+
+    # Finalize day
+    zorb.lifeness -= DAY_REDUCE_LIFENESS
+    zorb.energy -= zorb.consume / DAY_REDUCE_ENERGY
+    if zorb.lifeness < 0 or zorb.energy < 0:
+        zorb.alive = False
+
+
+def attack(world, zorb_a, zorb_b):
+    if round(zorb_a.attack, 2) == round(zorb_b.attack, 2):
+        # Even attact (Fight)
+        event_data = zorb_a.__dict__
+        event_data = zorb_b.__dict__
+        Event(event_type=EventType.Fight, data=event_data)
+
+        # Both alive, both lose energy, but zorb_a lose 60% extra
+        zorb_a.energy -= (zorb_a.consume * 1.6) / DAY_REDUCE_ENERGY
+        zorb_b.energy -= zorb_b.consume / DAY_REDUCE_ENERGY
+
+        # Both lose liveness
+        zorb_a.lifeness -= DAY_REDUCE_LIFENESS
+        zorb_b.lifeness -= DAY_REDUCE_LIFENESS
+    elif zorb_a.attack > zorb_b.attack:
+        # Stronger attact (Hunt)
+        event_data = zorb_a.__dict__
+        event_data = zorb_b.__dict__
+        Event(event_type=EventType.Hunt, data=event_data)
+
+        print("Zorb `{}` hunted zid:`{}` ".format(zorb_a.zid, zorb_a.zid))
+
+        # zorb_a alive only, zorb_a recovers 80% of zorb_b energy, liveness slightly
+        zorb_a.energy += zorb_b.energy * 0.6
+
+        # zorb_b dead
+        zorb_b.alive = False
+        pass
+    else:
+        #  Weaker attact
+
+        # zorb_a P alive (zorb_b.attack / zorb_a.defense)
+        if (zorb_b.attack / zorb_a.defense) >= 1:
+            zorb_a.alive = True
+            # zorb_a, if alive, lose enery, lose liveness sensible
+            zorb_a.energy -= (zorb_a.consume * 1.8) / DAY_REDUCE_ENERGY
+            zorb_a.lifeness -= DAY_REDUCE_LIFENESS * 2
+
+            event_data = zorb_a.__dict__
+            event_data = zorb_b.__dict__
+            Event(event_type=EventType.Fight, data=event_data)
+        else:
+            zorb_a.alive = False
+            event_data = zorb_a.__dict__
+            event_data = zorb_b.__dict__
+            Event(event_type=EventType.Kill, data=event_data)
+
+        # zorb_b alive, lose energy, liveness slightly
+        zorb_b.lifeness -= DAY_REDUCE_LIFENESS
+        zorb_a.energy -= (zorb_a.consume * 1.2) / DAY_REDUCE_ENERGY
+
+
+def reproduce(world, zorb_a, zorb_b):
+    """Share 50% energy of both parents
+    Creates 3 sons with the avg of all attributes
+    """
+
+    event_data = zorb_a.__dict__
+    event_data = zorb_b.__dict__
+    Event(event_type=EventType.Reproduced, data=event_data)
+
+    avgs = {
+        k: (getattr(zorb_a, k) + getattr(zorb_b, k))/2
+        for k in Zorb.INMUTABLE_ATTRIBUTES
+    }
+    zorb_a.energy, zorb_b.energy = zorb_a.energy * .5, zorb_b.energy * .5
+    avgs["energy"] = (zorb_a.energy + zorb_b.energy) / 3
+    [Zorb.birth(world, avgs) for _ in range(3)]
+
+
+def play(world):
+    """Play 1 day of this world
+    """
+    for zorb in get_zorbs():
+        zorb_day(world, zorb)
---
+++ game/zorb.py
@@ -0,0 +1,142 @@
+import uuid
+import random
+
+from .inout import get_db
+from .event import Event, EventType
+from .constants import (
+    ZORB_INITIAL_LIFENESS,
+    REDIS_ZORBS,
+    REDIS_ZORBS_PROCESSED,
+    REDIS_ZORBS_PROCESSING,
+    REDIS_ZORB_PREFIX
+)
+
+
+class Zorb(object):
+
+    INMUTABLE_ATTRIBUTES = ("defense", "attack", "speed")
+
+    def __init__(self, **kwargs):
+        for attribute, value in kwargs.items():
+            setattr(self, attribute, value)
+
+    def __del__(self):
+        if not hasattr(self, "zid"):
+            return None
+        key = self.zid_key()
+        db = get_db()
+
+        if self.alive:
+            # Update in Redis
+            db.hmset(key, self.__dict__)
+        else:
+            print("Dead zid:`{}` born :`{}`".format(self.zid, self.born))
+
+            # Create event
+            Event(event_type=EventType.Dead, data=self.__dict__)
+
+            # Remove from Redis
+            db.delete(key)
+            db.srem(REDIS_ZORBS, key)
+            db.srem(REDIS_ZORBS_PROCESSED, key)
+            db.srem(REDIS_ZORBS_PROCESSING, key)
+
+    @property
+    def weight(self):
+        # Weight (based upon defense x attack)
+        return self.defense + self.attack
+
+    @property
+    def consume(self):
+        # Consume (Energy consume based upon speed x weight)
+        return self.speed * self.weight
+
+    @property
+    def dna(self):
+        try:
+            return self._dna
+        except AttributeError:
+            self._dna = "".join((
+                bin(int(self.defense)),
+                bin(int(self.attack)),
+                bin(int(self.speed)),
+            ))
+            return self._dna
+
+    @classmethod
+    def birth(cls, world, kwargs):
+        kwargs["lifeness"] = ZORB_INITIAL_LIFENESS
+        kwargs["zid"] = str(uuid.uuid1())
+        kwargs["alive"] = True
+        kwargs["born"] = world.day
+
+        for k in cls.INMUTABLE_ATTRIBUTES:
+            kwargs[k] = kwargs[k] + random.gauss(0, .1)
+
+        obj = cls(**kwargs)
+
+        # Create in Redis
+        obj.dehidratate()
+
+        # Event register
+        Event(event_type=EventType.Born, data=obj.__dict__)
+
+        return obj
+
+    @classmethod
+    def hidratate(cls, key):
+        db = get_db()
+        kwargs = {}
+
+        for key, value in db.hgetall(key).items():
+            key, value = key.decode("utf-8"), value.decode("utf-8")
+            if key == "alive":
+                value = True
+            elif key in ("defense", "attack", "speed", "energy", "lifeness"):
+                value = float(value)
+            kwargs[key] = value
+
+        return cls(**kwargs)
+
+    def zid_key(self):
+        return "{}{}".format(REDIS_ZORB_PREFIX, self.zid)
+
+    def dehidratate(self):
+        db = get_db()
+        key = self.zid_key()
+        # Add zorb data
+        db.hmset(key, self.__dict__)
+        # Set zorb into set living zorbs
+        db.sadd(REDIS_ZORBS, key)
+
+
+def get_zorbs():
+    def restart_queue(db):
+        # Iteration ended
+        if db.scard(REDIS_ZORBS_PROCESSING) == 0 and db.scard(REDIS_ZORBS_PROCESSED) == 0:
+            # Copy main set into processing set
+            db.sunionstore(REDIS_ZORBS_PROCESSING, REDIS_ZORBS)
+            db.sunionstore(REDIS_ZORBS_PROCESSED, REDIS_ZORBS)
+
+        # Processing error, restore from processed
+        elif db.scard(REDIS_ZORBS_PROCESSING) == 0 and db.scard(REDIS_ZORBS_PROCESSED) > 0:
+            db.sunionstore(REDIS_ZORBS_PROCESSING, REDIS_ZORBS_PROCESSED)
+
+    db = get_db()
+
+    # Restart queue if needed
+    restart_queue(db)
+
+    while True:
+        zorb = db.spop(REDIS_ZORBS_PROCESSING)
+        if not zorb:
+            raise StopIteration()
+        zorb = Zorb.hidratate(zorb.decode('utf-8'))
+        yield zorb
+        db.srem(REDIS_ZORBS_PROCESSED, zorb.zid)
+
+
+def get_random_zorb():
+    db = get_db()
+    zorb = db.srandmember(REDIS_ZORBS)
+    return Zorb.hidratate(zorb.decode('utf-8'))
