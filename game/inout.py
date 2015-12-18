75450afe3329eb31460f5380b587a324396a48da
Tolo Palmer <toloco@gmail.com>
2015-12-18 07:45:08 +0000

    The rest

---
+++ game/inout.py
@@ -0,0 +1,18 @@
+import os
+import json
+import redis
+
+from .constants import ENV_REDIS_CONF
+
+
+conf = json.loads(os.getenv(ENV_REDIS_CONF, """{
+    "host": "127.0.0.1",
+    "port": 6379,
+    "db": 0
+}"""))
+pool = redis.ConnectionPool(**conf)
+
+
+def get_db():
+    """Returns DB connection"""
+    return redis.Redis(connection_pool=pool)
