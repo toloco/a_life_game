a7b2882720955aeffbf7f6979603cbe827108f61
Tolo Palmer <toloco@gmail.com>
2015-12-18 07:45:21 +0000

    The events

---
+++ game/event.py
@@ -0,0 +1,44 @@
+import uuid
+from enum import Enum, unique
+
+from .inout import get_db
+from .world import World
+from .constants import (
+    REDIS_EVENTS,
+    REDIS_EVENTS_TYPE,
+    REDIS_EVENTS_PREFIX
+)
+
+
+@unique
+class EventType(Enum):
+    Born = 1
+    Reproduced = 2
+    Dead = 3
+    Hunt = 4
+    Kill = 5
+    Fight = 6
+    Feed = 7
+
+    def __str__(self):
+        return self.name
+
+
+class Event(object):
+
+    def __init__(self, event_type, data):
+        self.id = str(uuid.uuid1())
+        self.type = event_type
+        self.data = data
+        self.data["day"] = World().day
+
+    def __del__(self):
+        db = get_db()
+        key = REDIS_EVENTS_PREFIX.format(
+            type=self.type,
+            id=self.id
+        )
+
+        db.hmset(key, self.data)
+        db.sadd(REDIS_EVENTS, key)
+        db.sadd(REDIS_EVENTS_TYPE.format(self.type), key)
